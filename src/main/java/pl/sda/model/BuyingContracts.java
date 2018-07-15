c1338d5799363ecf467bb1811663f69ca00ff6d0
wozog <hugowojtek@o2.pl>
2018-07-15 17:26:22 +0200

    jn

--- src/main/java/pl/sda/model/BuyingContracts.java
+++ src/main/java/pl/sda/model/BuyingContracts.java
@@ -2,6 +2,8 @@ package pl.sda.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -11,6 +13,28 @@ public class BuyingContracts extends BaseModel implements Serializable{
     private Date date;
     @Column
     private Long price;
+    @OneToOne
+    @JoinColumn(name="idcars")
+    private Cars cars;
+    @OneToOne
+    @JoinColumn(name="idcustomers")
+    private Cutomers cutomers;
+
+    public Cars getCars() {
+        return cars;
+    }
+
+    public void setCars(Cars cars) {
+        this.cars = cars;
+    }
+
+    public Cutomers getCutomers() {
+        return cutomers;
+    }
+
+    public void setCutomers(Cutomers cutomers) {
+        this.cutomers = cutomers;
+    }
 
     public Date getDate() {
         return date;
