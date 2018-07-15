11937f9e25b8e8df32abb8c51ddc0ee311eda0fd
wozog <hugowojtek@o2.pl>
2018-07-15 11:27:49 +0200

    wyswietla wszystkie samochody z bazy wraz z opisami

--- src/main/java/pl/sda/controller/ShowAllCars.java
+++ src/main/java/pl/sda/controller/ShowAllCars.java
@@ -3,6 +3,7 @@ package pl.sda.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pl.sda.Repository.CarsRepository;
@@ -27,4 +28,22 @@ public class ShowAllCars {
         model.addAttribute("cars1",cars);
         return "allcars";
     }
+
+    @RequestMapping("/{carId}/desc")
+    public String getCarDescription(
+            @PathVariable("carId") Long carId, Model model){
+        Cars car = carsRepository.findOne(carId);
+        String description = car.getDescription();
+        model.addAttribute("carDesc",description);
+        return "description";
+    }
+//
+//    @RequestMapping("/{vehicleId}/sell")
+//    public String sellVehicleForm(
+//            @PathVariable("vehicleId") Long vehicleId,
+//            Model model){
+//        model.addAttribute("vehicleId",vehicleId);
+//        model.addAttribute("sellData",new PurchaseFormData());
+//        return "sellVehicle";
+//    }
 }
