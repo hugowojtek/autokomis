8246d935a16bd19f1a2412394dd259decd061da9
wozog <hugowojtek@o2.pl>
2018-07-19 22:40:29 +0200

    jn + raports full completed

--- src/main/java/pl/sda/controller/Raports.java
+++ src/main/java/pl/sda/controller/Raports.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import pl.sda.model.Cars;
 import pl.sda.model.DtoShowCar;
 import pl.sda.service.CarsService;
+import pl.sda.service.RaportsService;
 
 import java.util.List;
 
@@ -15,19 +16,24 @@ import java.util.List;
 public class Raports {
 
     CarsService carsService;
+    RaportsService raportsService;
 
-    public Raports(CarsService carsService) {
+    public Raports(CarsService carsService, RaportsService raportsService) {
         this.carsService = carsService;
+        this.raportsService = raportsService;
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public String ShowSumOfBoughtCars(Model model){
         List<DtoShowCar> list1 = carsService.showBoughtCars();
         List<DtoShowCar> list2 = carsService.showAvailableCars();
-        List<DtoShowCar> list3 = carsService.showSoldCars();
+        //List<DtoShowCar> list3 = carsService.showSoldCars();
+        List<DtoShowCar> list3 = raportsService.ShowSoldCarsWithMarginAndProfit();
+        Long value = raportsService.CalculateAllProfit();
         model.addAttribute("cars1",list1);
         model.addAttribute("cars2",list2);
         model.addAttribute("cars3",list3);
+        model.addAttribute("value",value);
         return "fullRaports";
     }
 
--- src/main/java/pl/sda/service/RaportsService.java
+++ src/main/java/pl/sda/service/RaportsService.java
@@ -4,18 +4,67 @@ import org.springframework.stereotype.Service;
 import pl.sda.Repository.BuyingContractsRepository;
 import pl.sda.Repository.CarsRepository;
 import pl.sda.Repository.SellingContractsRepository;
+import pl.sda.model.Cars;
+import pl.sda.model.DtoShowCar;
+import pl.sda.model.SellingContracts;
+
+import java.util.List;
+
+import static java.lang.Math.round;
 
 @Service
 public class RaportsService {
-    BuyingContractsRepository buyingContractsRepository;
-    CarsRepository carsRepository;
-    SellingContractsRepository sellingContractsRepository;
 
-    public RaportsService(BuyingContractsRepository buyingContractsRepository, CarsRepository carsRepository, SellingContractsRepository sellingContractsRepository) {
+
+    CarsService carsService;
+    final BuyingContractsRepository buyingContractsRepository;
+    final CarsRepository carsRepository;
+    final SellingContractsRepository sellingContractsRepository;
+
+    public RaportsService(CarsService carsService, BuyingContractsRepository buyingContractsRepository, CarsRepository carsRepository, SellingContractsRepository sellingContractsRepository) {
+        this.carsService = carsService;
         this.buyingContractsRepository = buyingContractsRepository;
         this.carsRepository = carsRepository;
         this.sellingContractsRepository = sellingContractsRepository;
     }
+
+
+    public List<DtoShowCar> ShowSoldCarsWithMarginAndProfit() {
+        List<DtoShowCar> soldCars = carsService.showSoldCars();
+        List<DtoShowCar> boughtCars = carsService.showBoughtCars();
+        for (DtoShowCar sc : soldCars) {
+            for (DtoShowCar bc : boughtCars) {
+                if (sc.getId().equals(bc.getId())) {
+                    Long profit = sc.getCarPrice() - bc.getCarPrice();
+                    sc.setProfit(profit);
+                    float margin = (((float)profit / sc.getCarPrice()) * 100);
+                    float margin_round = round(margin);
+                    sc.setMargin(margin_round);
+                }
+            }
+        }
+        return soldCars;
+    }
+
+    public Long CalculateAllProfit(){
+        List<DtoShowCar> soldCars = carsService.showSoldCars();
+        List<DtoShowCar> boughtCars = carsService.showBoughtCars();
+        Long value=0L;
+        for (DtoShowCar sc : soldCars) {
+            for (DtoShowCar bc : boughtCars) {
+                if (sc.getId().equals(bc.getId())) {
+                    Long profit = sc.getCarPrice() - bc.getCarPrice();
+                    sc.setProfit(profit);
+                    float margin = (((float)profit / sc.getCarPrice()) * 100);
+                    sc.setMargin(margin);
+                    value+=sc.getCarPrice();
+                }
+            }
+        }
+        return value;
+    }
+
+
 
 
 }
