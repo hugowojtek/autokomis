c12ea604d5b381a232d1d7eeec76febf3a0126c5
wozog <hugowojtek@o2.pl>
2018-07-27 08:52:50 +0200

    wersja 1 back and front

--- src/main/java/pl/sda/controller/LoginController.java
+++ src/main/java/pl/sda/controller/LoginController.java
@@ -2,14 +2,21 @@ package pl.sda.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/login")
 public class LoginController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String login() {
         return "login";
+    }
+
+    @PostMapping
+    public String gocars() {
+        return "availableCars";
     }
 }
